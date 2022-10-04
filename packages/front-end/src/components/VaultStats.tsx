b3018cd4fb6c20e46d45dd1b56bf79bd3ead3549
ugolino <dan.ugolini@gmail.com>
2022-10-04 15:35:21 +0200

    wip

--- packages/front-end/src/components/VaultStats.tsx
+++ packages/front-end/src/components/VaultStats.tsx
@@ -1,19 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import NumberFormat from "react-number-format";
 import { Card } from "./shared/Card";
 import ReactTooltip from "react-tooltip";
 import { RyskTooltip } from "./RyskTooltip";
 import { DHV_NAME } from "../config/constants";
+import { gql, useQuery } from "@apollo/client";
 
 export const VaultStats = () => {
+
+  const [cumulativeYield, setcumulativeYield] = useState(0);
+
+
+  const getMetrics = (data: any) => {
+
+
+    console.log(data)
+
+    setcumulativeYield(100);
+  };
+
+    useQuery(
+    gql`
+    query($account: String) {
+      dailyStatSnapshots(first: 1000, orderBy: timestamp, orderDirection: "asc") {
+        id
+        cumulativeYield
+        totalAssets
+        totalReturns
+      }
+    }
+  `,
+    {
+      onCompleted: getMetrics,
+      onError: (err) => {
+        console.log(err);
+      },
+    }
+  );
+
   return (
     <div className="pb-8 py-12 px-8 flex flex-col lg:flex-row h-full">
       <div className="flex h-full w-full justify-around">
         <div className="flex flex-col items-left justify-center h-full mb-8 lg:mb-0">
           <p className="mb-2 text-xl">
             Cumulative Yield:{" "}
-            {/* <NumberFormat
-              value={""}
+            <NumberFormat
+              value={cumulativeYield}
               displayType={"text"}
               decimalScale={2}
               suffix="%"
@@ -21,9 +53,9 @@ export const VaultStats = () => {
             />
             <RyskTooltip
               id="yieldTip"
-              message={`Sum of ${DHV_NAME} returns since inception (Jul 1st 2022)`}
-            /> */}
-            Soon™️
+              message={`Sum of ${DHV_NAME} returns since inception (Sept 30th 2022)`}
+            />
+            {/* Soon™️ */}
           </p>
           {/* <a
             href="https://docs.rysk.finance"
