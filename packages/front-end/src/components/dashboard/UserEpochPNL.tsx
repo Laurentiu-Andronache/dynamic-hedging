cc8ad0c1fd8b4564d36bd0b328daadf8b0df01cb
Yassine <yassinesania@gmail.com>
2022-10-26 12:17:48 +0200

    Group user PNL by day
    + scale based on time
    + add back CartesianGrid

--- packages/front-end/src/components/dashboard/UserEpochPNL.tsx
+++ packages/front-end/src/components/dashboard/UserEpochPNL.tsx
@@ -11,6 +11,7 @@ import {
     Tooltip,
     ResponsiveContainer,
     Legend,
+    CartesianGrid
 } from "recharts";
 import {Card} from "../shared/Card";
 import {useWalletContext} from "../../App";
@@ -23,7 +24,7 @@ interface CustomTooltipT {
 
 const CustomTooltip = ({ active, payload, label }: CustomTooltipT) => {
     if (label && active && payload && payload.length) {
-        const date = new Date(label)
+        const date = new Date(parseInt(label) * 1000)
         return (
             <div className="custom-tooltip bg-bone bg-opacity-75 border-black p-4 border-2 border-b-2 rounded-xl border-black">
                 <p>
@@ -99,8 +100,12 @@ export const UserEpochPNL = () => {
                     data.pricePerShares.map((ppsEpoch : any, i : number, values : any) => {
                         console.log(`EPOCH ${i}`)
 
-                        const date = new Date(parseInt(ppsEpoch.timestamp) * 1000)
-                        const dateLocale = date.toLocaleString('default', {month: 'numeric', day: 'numeric', year: 'numeric' })
+                        const dateLocale = (new Date(parseInt(ppsEpoch.timestamp) * 1000))
+                            .toLocaleString('default', {
+                                month: 'numeric',
+                                day: 'numeric',
+                                year: 'numeric'
+                            })
 
                         console.log('Date Locale: ', dateLocale);
 
@@ -141,13 +146,50 @@ export const UserEpochPNL = () => {
                         tempPNL.push({
                             shares: s.toString(),
                             pnl: totalPNL.toString(),
-                            timestamp: date.toISOString(),
+                            timestamp: ppsEpoch.timestamp,
                             dateLocale: dateLocale,
                             epoch: ppsEpoch.id,
                         });
                     })
 
-                tempPNL.length > 0 && setHistoricalPNL(tempPNL);
+                const refinedDataByDate = tempPNL
+                    .reduce((mapByDate: any, nextEpoch: any) => {
+                        const dateLocale = nextEpoch.dateLocale
+
+                        if (!(dateLocale in mapByDate)) {
+                            mapByDate[dateLocale] = {
+                                ...nextEpoch,
+                                epoch: [ nextEpoch.epoch ]
+                            }
+                            return mapByDate
+                        }
+
+                        // in case there is already same day We merge values
+
+                        const dateGroup = mapByDate[dateLocale];
+
+                        mapByDate[dateLocale] = {
+                            epoch: [ ...dateGroup.epoch, nextEpoch.epoch ],
+                            // We keep the more recent pnl
+                            pnl: nextEpoch.timestamp > dateGroup.timestamp ?
+                                nextEpoch.pnl : dateGroup.pnl,
+                            // Sum share deposits
+                            shares:
+                                BigNumber.from(nextEpoch.shares).add(BigNumber.from(dateGroup.shares)).toString(),
+                            // We keep latest timestamp
+                            timestamp: Math.max(nextEpoch.timestamp, dateGroup.timestamp),
+                            // It's the same day
+                            dateLocale
+                        }
+
+                        return mapByDate
+                    }, { })
+
+                console.log('Refined Data By Date: ', refinedDataByDate)
+
+                Object.keys(refinedDataByDate).length > 0 && setHistoricalPNL(Object.values(refinedDataByDate));
+
+                // tempPNL.length > 0 && setHistoricalPNL(tempPNL);
             },
         }
     );
@@ -175,11 +217,21 @@ export const UserEpochPNL = () => {
                                                 // TODO access color throw Tailwind helpers
                                                 stroke="black"
                                                 strokeWidth={2}
+                                                dot={false}
                                             />
-                                            <XAxis dataKey="timestamp" angle={0} tickFormatter={(value: string) => {
-                                                const date = new Date(value)
-                                                return date.toLocaleString('default', { month: 'short', day:'2-digit' });
-                                            }} />
+                                            <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                                            <XAxis
+                                                type="number"
+                                                scale="time"
+                                                domain={['dataMin', 'dataMax']}
+                                                dataKey="timestamp"
+                                                angle={0}
+                                                tickFormatter={(value: string) => {
+                                                    const date = new Date(parseInt(value) * 1000)
+                                                    return date.toLocaleString('default', {
+                                                        month: 'short', day:'2-digit'
+                                                    });
+                                                }} />
                                             <YAxis />
                                             <Tooltip content={<CustomTooltip />} />
                                             <Legend
