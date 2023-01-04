8d612dbab66c26189cfacd688431587538ff6fa8
kjr217 <kjraja217@gmail.com>
2023-01-04 14:34:46 +0000

    feat: add opening timestamp to referral code contract

--- packages/contracts/contracts/ReferralCode.sol
+++ packages/contracts/contracts/ReferralCode.sol
@@ -7,8 +7,10 @@ contract ReferralCode is AccessControl {
 
     struct CodeDetails {
         address owner;
+        uint64 openingTimestamp;
         uint128 notionalTraded;
         uint128 premiumOriginated;
+
     }
 
     // mapping of referral code to code details struct
@@ -16,7 +18,7 @@ contract ReferralCode is AccessControl {
     // mapping of addresses authorised to update the details of a code
     mapping(address => bool) public auth;
 
-    event ReferralCodeCreated(bytes32 code);
+    event ReferralCodeCreated(bytes32 code, uint256 openingTimestamp);
     event ReferralCodeOwnerChanged(bytes32 code, address newOwner);
     event ReferralCodeDetailsUpdated(bytes32 code, uint256 notionalTraded, uint256 premiumOriginated);
     event AuthChanged(address auth, bool isAuth);
@@ -24,7 +26,7 @@ contract ReferralCode is AccessControl {
     error NotAuth();
 
 	constructor(address _authority) AccessControl(IAuthority(_authority)) {}
-    
+
     function setAuth(address _auth, bool isAuth) external {
         _onlyGovernor();
         auth[_auth] = isAuth;
@@ -35,7 +37,8 @@ contract ReferralCode is AccessControl {
         require(code != bytes32(0), "ReferralCode: invalid code");
         require(codeDetails[code].owner == address(0), "ReferralCode: code already in use");
         codeDetails[code].owner = msg.sender;
-        emit ReferralCodeCreated(code);
+        codeDetails[code].openingTimestamp = uint64(block.timestamp);
+        emit ReferralCodeCreated(code, block.timestamp);
     }
 
     function setCodeOwner(bytes32 code, address owner) external {
